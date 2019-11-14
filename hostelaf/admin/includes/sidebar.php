29522a27b6e047d9c12999034bf3dc8713380733
abdmuiz47 <49932801+abdmuiz47@users.noreply.github.com>
2019-11-14 14:49:57 +0800

    Project SCM

---
+++ hostelaf/admin/includes/sidebar.php
@@ -0,0 +1,24 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					<li><a href="#"><i class="fa fa-files-o"></i> Courses</a>
+					<ul>
+						<li><a href="add-courses.php">Add Courses</a></li>
+						<li><a href="manage-courses.php">Manage Courses</a></li>
+					</ul>
+				</li>
+					<li><a href="manage-rooms.php"><i class="fa fa-desktop"></i> Manage Rooms</a>
+				</li>
+
+				<li><a href="registration.php"><i class="fa fa-user"></i>Room & Student Registration</a>
+					<ul>
+						<li><a href="registration.php">Student Registration for Room</a></li>
+						<li><a href="registrationstudent.php">Register Student</a></li>
+					</ul>
+				</li>
+				<li><a href="access-log.php"><i class="fa fa-file"></i>User Access logs</a></li>
+
+			
+		</nav>
